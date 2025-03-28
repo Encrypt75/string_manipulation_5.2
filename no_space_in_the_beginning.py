701011c89955965c5042c09d2e20819b864e016f
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-28 19:42:50 +0800

    no_space_in_the_beginning_6

---
+++ no_space_in_the_beginning.py
@@ -0,0 +1,6 @@
+#lstrip() remove the space characters at the beginning of the string. 
+#Create a program that do the same functionality without using lstrip() function.
+
+ask_string_input = input("Enter text/messege:\n")
+
+print(ask_string_input)
