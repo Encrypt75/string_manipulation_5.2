7bc3d611bb505d2f9930b892c697769f685fc4f5
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-28 20:22:05 +0800

    no_space_in_the_beginning_6
    This program removes all unnecessary spaces in front of a string.

--- no_space_in_the_beginning.py
+++ no_space_in_the_beginning.py
@@ -7,10 +7,18 @@ character = []
 #ask for string input
 ask_string_input = input("Enter text/messege: ")
 
+#checks if every character is an alpabet
 for every_char in ask_string_input:
     if every_char.isalpha():
         character.append(every_char)
     else: 
         pass
 
-print(character)
+#convert list into string
+lst_to_strng = " ".join(character)
+
+#remove spaces in between the str
+remove_space = lst_to_strng.replace(" ", "")
+
+#print statement
+print(remove_space)
