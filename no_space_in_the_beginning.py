d258f3df7b371393b77ed7b7f45069cdc61dec85
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-28 20:07:44 +0800

    no_space_in_the_beginning_6.1
    For progress: for loop was introduced and checks if the character is an alphabet or not

--- no_space_in_the_beginning.py
+++ no_space_in_the_beginning.py
@@ -1,6 +1,18 @@
 #lstrip() remove the space characters at the beginning of the string. 
 #Create a program that do the same functionality without using lstrip() function.
 
-ask_string_input = input("Enter text/messege:\n")
+#ask for string input
+ask_string_input = input("Enter text/messege: ")
 
-print(ask_string_input)
+for every_char in ask_string_input:
+    if every_char.isalpha():
+        print(f"{every_char} = True")
+    else: 
+        print(f"{every_char} = False")
+
+#ask user input
+#for loop checking for each character
+    #if char is not alphabet
+        #replace(" ", "")
+    #else:
+        #pass
