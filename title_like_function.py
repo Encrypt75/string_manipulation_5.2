aaf1f450a39fde282d2e92637e60397ecd977408
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-29 14:54:26 +0800

    title_like_function_6
    This program acts like a .title function without using python's title function

--- title_like_function.py
+++ title_like_function.py
@@ -1,12 +1,19 @@
 #title() makes all first letter of each word in the string, capital letter. And all other letter in small case. 
 #Create a program that do the same functionality without using title() function.
 
+#ask user for an input
 string_input = input("Enter any text: ")
-             
+
+#storage for every word            
+result = []
+
+#this fixes which one to capitalize and which are not
 for word in string_input.split():
     if word:
-        result = "".join(word[0].upper() + word[1:].lower())
-    else:
-        print(" ")
-        
-print(result)
+        result.append(word[0].upper() + word[1:].lower())  
+
+#join to convert list to string
+final_result = " ".join(result)
+
+#print statement
+print(final_result)
