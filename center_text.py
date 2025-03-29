47f2df3674c290172f8b87e8306128b781de167f
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-29 17:09:53 +0800

    center_text_6
    This program prints the text in the middle as if it is using the center function.

--- center_text.py
+++ center_text.py
@@ -1,14 +1,13 @@
 #center() add space characters at the beginning and at the end of the string to print the string at the center. 
 #Create a program that do the same functionality without using center() function.
 
-#ask for input
-ask_text = input("Enter any text: ")
+ask_text = input("Enter any text:")
 
 #check how many character are there
 char_count = len(ask_text)
 
 #subtruct the total number of character to 99 (the middle)
-num_of_spaces = 99 - char_count
+num_of_spaces = 98 - char_count // 2
 
 #print statement
 print(num_of_spaces * " ", ask_text)
